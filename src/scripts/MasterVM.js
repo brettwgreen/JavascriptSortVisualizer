dbd9c76ef37fce757456e5a964cbd27de8eae8dd
Brett Green <bgreen@fdsrx.com>
2016-12-12 11:33:32 -0500

    javascript prettified

--- src/scripts/MasterVM.js
+++ src/scripts/MasterVM.js
@@ -1,16 +1,10 @@
 $(document).ready(function() {
-
-
-var MasterVm = function() {
-	var self = this;
-	self.quickSort = new QuickSortVM();
-	self.bubbleSort = new BubbleSortVM();
-	self.selectionSort = new SelectionSortVM();
-	
-
-};
-
-var vm = new MasterVm();
-ko.applyBindings(vm);
+	var MasterVm = function() {
+		var self = this;
+		self.quickSort = new QuickSortVM();
+		self.bubbleSort = new BubbleSortVM();
+		self.selectionSort = new SelectionSortVM();
+	};
+	var vm = new MasterVm();
+	ko.applyBindings(vm);
 });
-
--- src/scripts/SelectionSort.js
+++ src/scripts/SelectionSort.js
@@ -1,78 +1,77 @@
-
-var SS_Unit = function(val) {
-  var self = this;
-  self.value = ko.observable(val);
-  self.inspecting = ko.observable(false);
-  self.currentStart = ko.observable(false);
+var BS_Unit = function(val) {
+	var self = this;
+	self.value = ko.observable(val);
+	self.inspecting = ko.observable(false);
+	self.currentStart = ko.observable(
+		false);
 }
-
-var SelectionSortVM = function() {
+var BubbleSortVM = function() {
 	var self = this;
 	self.data = ko.observableArray([]);
 	self.elementCount = 50;
 	self.delay = ko.observable(10);
-	
 	self.init = function() {
 		var count = 0;
 		var nums = [];
 		var numData = [];
 		while (count < self.elementCount) {
-			var num = Math.ceil(Math.random() * self.elementCount);
+			var num = Math.ceil(Math.random() *
+				self.elementCount);
 			if (!nums.includes(num)) {
-		nums.push(num);
-		numData.push(new SS_Unit(num));
+				nums.push(num);
+				numData.push(new BS_Unit(num));
 				count++;
 			}
 		}
-	  self.data(numData);
+		self.data(numData);
 	};
-
-  self.swap = function(targetIndex, sourceIndex) {
-	var tempValue = self.data()[targetIndex].value();
-	self.data()[targetIndex].value(self.data()[sourceIndex].value())
-	self.data()[sourceIndex].value(tempValue);
-  };
-  
-  self.checkSmallest = function(i) {
-	if (self.data()[i].value() < self._smallest) {
-	  self._smallest = self.data()[i].value();
-	  self._smallestIndex = i;
+	self.swap = function(targetIndex,
+		sourceIndex) {
+		var tempValue = self.data()[
+			targetIndex].value();
+		self.data()[targetIndex].value(self
+			.data()[sourceIndex].value())
+		self.data()[sourceIndex].value(
+			tempValue);
+	};
+	self.doTheBubble = function(i) {
+		if (i + 1 <= self.data().length - 1) {
+			if (self.data()[i].value() > self.data()[
+					i + 1].value()) {
+				self.swap(i, i + 1);
+				self._swapOccurred = true;
+			}
+		}
+		self.data()[i].inspecting(false);
+		self.data()[i].currentStart(false);
+		if (i + 1 <= self.data().length - 1) {
+			self.data()[i + 1].currentStart(
+				true);
+			self.data()[i + 1].inspecting(true);
+			setTimeout(self.doTheBubble, self.delay(),
+				i + 1);
+		} else {
+			self.completeBubbleSortLoop();
+		}
+	};
+	self.startBubbleSort = function() {
+		self._swapOccurred = false;
+		self._startingIndex = 0;
+		self.data()[self._startingIndex].inspecting(
+			true);
+		self.data()[self._startingIndex].currentStart(
+			true);
+		setTimeout(self.doTheBubble, self.delay(),
+			self._startingIndex);
+	};
+	self.completeBubbleSortLoop =
+		function() {
+			if (self._swapOccurred) {
+				self.startBubbleSort();
+			}
+		};
+	self.bubbleSort = function() {
+		self.startBubbleSort();
 	}
-	self.data()[i].inspecting(false);
-	if (i+1 <= self.data().length-1) {
-	  self.data()[i+1].inspecting(true);
-	  setTimeout(self.checkSmallest, self.delay(), i+1);
-	}
-	else {
-	  self.completeSelectionSortFrom();
-	}
-  }
-  
-  self.startSelectionSortFrom = function(i) {
-	  if (i > self.data().length-1) {
-		return;
-	  }
-	  var delay = 50;
-	self._smallestIndex = i;
-	self._startingIndex = i;
-	self._smallest = self.data()[self._smallestIndex].value();
-	self.data()[self._startingIndex].inspecting(true);
-	self.data()[self._startingIndex].currentStart(true);
-	setTimeout(self.checkSmallest, self.delay(), self._startingIndex);
-  };
-
-  self.completeSelectionSortFrom = function() {
-	console.log('smallest: ' + self._smallest + '[' + self._smallestIndex + ']');
-	self.swap(self._startingIndex, self._smallestIndex);
-	self.data()[self._startingIndex].currentStart(false);
-	self.startSelectionSortFrom(self._startingIndex+1);
-  };
-  
-  self.selectionSort = function() {
-	self.startSelectionSortFrom(0);
-  }
-	
-	
-  self.init();
+	self.init();
 };
-

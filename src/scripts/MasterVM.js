a4405b5bed7baf4b4440b213805f8721c1743430
Brett Green <bgreen@fdsrx.com>
2016-12-12 14:13:55 -0500

    add nearly sorted option

--- src/scripts/MasterVM.js
+++ src/scripts/MasterVM.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 	var MasterVm = function() {
 		var self = this;
-		self.elementCount = 50;
-		self.getRandomArray = function(num) {
+		self.getRandomArray = function(numberOfElements) {
 			var count = 0;
+			numberOfElements = (numberOfElements) ? numberOfElements : 50;
 			var nums = [];
-			while (count < self.elementCount) {
-				var num = Math.ceil(Math.random() * self.elementCount);
+			while (count < numberOfElements) {
+				var num = Math.ceil(Math.random() * numberOfElements);
 				if (!nums.includes(num)) {
 					nums.push(num);
 					count++;
@@ -22,10 +22,30 @@ $(document).ready(function() {
 			}
 			return nums;
 		};		
+		self.getNearlySortedArray = function(numberOfElements) {
+			var nums = [];
+			var current = 0;
+			while (current < numberOfElements) {
+				nums.push(current);
+				current++
+			}
+			var swapTotal = 5;
+			var swaps = 0;
+			while (swaps < swapTotal) {
+				var num1 = Math.ceil(Math.random() * numberOfElements);
+				var num2 = Math.ceil(Math.random() * numberOfElements);
+				if (num1 !== num2) {
+					var temp = nums[num2];
+					nums[num2] = nums[num1];
+					nums[num1] = temp;
+					swaps++;
+				}
+			}
+			return nums;
+		};				
 		self.quickSort = new QuickSortVM(self.getRandomArray);
 		self.bubbleSort = new BubbleSortVM(self.getRandomArray);
 		self.selectionSort = new SelectionSortVM(self.getRandomArray);
-		
 		self.startAll = function() {
 			self.quickSort.sort();
 			self.bubbleSort.sort();
@@ -36,21 +56,27 @@ $(document).ready(function() {
 			self.bubbleSort.init();
 			self.selectionSort.init();
 		};
-		
 		self.scrambleAllSame = function() {
 			var data = self.getRandomArray(50);
 			self.selectionSort.setData(data);
 			self.bubbleSort.setData(data);
 			self.quickSort.setData(data);
 		};
-		
 		self.setDescending = function() {
 			var data = self.getDescendingArray(50);
 			self.selectionSort.setData(data);
 			self.bubbleSort.setData(data);
 			self.quickSort.setData(data);
 		};
+		self.setNearlySorted = function() {
+			var data = self.getNearlySortedArray(50);
+			self.selectionSort.setData(data);
+			self.bubbleSort.setData(data);
+			self.quickSort.setData(data);
+		};
+		
 	};
+	
 	var vm = new MasterVm();
 	ko.applyBindings(vm);
 });
